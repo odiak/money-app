279d1240226c38ba9c8025454eaad53d6ba15c91
Kaido Iwamoto <h6824.kaido@gmail.com>
2015-02-17 01:50:19 +0900

    Fix unicorn.rb

--- unicorn.rb
+++ unicorn.rb
@@ -1,11 +1,12 @@
 root_dir = File.absolute_path(File.dirname(__FILE__))
+tmp_dir = "#{root_dir}/tmp"
 
 worker_processes 2
 working_directory root_dir
 
 timeout 300
-listen "tmp/unicorn.sock"
-pid "tmp/unicorn.pid"
+listen "#{tmp_dir}/unicorn.sock"
+pid "#{tmp_dir}/unicorn.pid"
 
-stderr_path "tmp/unicorn.stderr.log"
-stdout_path "tmp/unicorn.stdout.log"
+stderr_path "#{tmp_dir}/unicorn.stderr.log"
+stdout_path "#{tmp_dir}/unicorn.stdout.log"
