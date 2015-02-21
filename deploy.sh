74f68676746acdfa685c74d4d6e347d149615608
Kaido Iwamoto <h6824.kaido@gmail.com>
2015-02-22 02:12:30 +0900

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function invoke() {
+    echo $1
+    eval $1
+}
+
+invoke '[ -f ./tmp/unicorn.pid ] && kill -QUIT $(< ./tmp/unicorn.pid)'
+invoke 'git pull'
+invoke 'npm install'
+invoke 'NODE_ENV=production ./node_modules/gulp/bin/gulp.js'
+invoke 'bundle install --path vendor/bundle'
+invoke 'bundle exec unicorn -c unicorn.rb -E production -D'
