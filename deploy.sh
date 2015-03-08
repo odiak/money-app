43192d9fd345832da0045a84c2d602a28b4f9598
Kaido Iwamoto <h6824.kaido@gmail.com>
2015-03-08 12:54:51 +0900

    Modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+function sha1() {
+    ruby -e "require 'digest/sha1'; puts Digest::SHA1.hexdigest(open('$1').read)"
+}
+
 function invoke() {
     echo $1
     eval $1
 }
 
+checksum=$(sha1($0))
+
+if [ $WITHOUT_GIT_PULL == '' ]; then
+    invoke 'git pull origin master'
+    if [ $checksum != $(sha1($0)) ]; then
+        export WITHOUT_GIT_PULL=1
+        exec $0
+    fi
+fi
+
 invoke '[ -f ./tmp/unicorn.pid ] && kill -QUIT $(< ./tmp/unicorn.pid)'
-invoke 'git pull origin master'
 invoke 'npm install'
 invoke 'NODE_ENV=production ./node_modules/gulp/bin/gulp.js'
 invoke 'bundle install --path vendor/bundle'
